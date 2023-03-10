4e185eaa65b85e2f138a5a9f1f03ad05cef843d3
Rehman Ali <rehmanali.9442289@gmail.com>
2023-03-10 17:30:04 +0500

    FIX the version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ required_packages = []
 
 setuptools.setup(
     name="tweepy_exts",
-    version="1.0",
+    version="0.1.0",
     author="Rehman Ali",
     author_email="rehmanali.9442289@gmail.com",
     description="A wrapper for official tweepy for more simple usage",
